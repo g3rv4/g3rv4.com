97d05d4d392dcd39d58c54fee6b33a2952128cdf
Gervasio Marchand <gmc@gmc.uy>
2017-04-22 12:21:41 -0300

    Me no likey tag pages anymore

--- _plugins/data-page-generator.rb
+++ _plugins/data-page-generator.rb
@@ -48,6 +48,7 @@ module Jekyll
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), template + ".html")
       self.data['title'] = data[name]
+      self.data['istagpage'] = true
       # add all the information defined in _data for the current record to the
       # current page (so that we can access it with liquid tags)
       self.content = Kramdown::Document.new(self.content).to_html
